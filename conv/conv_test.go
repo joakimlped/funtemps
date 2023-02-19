dc968dab2244c15da67c30c4d865524b17cc0846
joakimlped <joakimlped@gmail.com>
2023-02-19 21:13:56 +0100

    testfilen

---
+++ conv/conv_test.go
@@ -0,0 +1,117 @@
+package conv
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFahrenheitToCelsius(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 134, want: 56.67},
+		// add more test cases here
+	}
+
+	for _, tc := range tests {
+		got := FahrenheitToCelsius(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("FahrenheitToCelsius(%v) = %v; want %v", tc.input, got, tc.want)
+		}
+	}
+}
+
+func TestCelsiusToFahrenheit(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 25, want: 77},
+		// add more test cases here
+	}
+
+	for _, tc := range tests {
+		got := CelsiusToFahrenheit(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("CelsiusToFahrenheit(%v) = %v; want %v", tc.input, got, tc.want)
+		}
+	}
+}
+
+func TestKelvinToFahrenheit(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 273.15, want: 32},
+		// add more test cases here
+	}
+
+	for _, tc := range tests {
+		got := KelvinToFahrenheit(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("KelvinToFahrenheit(%v) = %v; want %v", tc.input, got, tc.want)
+		}
+	}
+}
+
+func TestFahrenheitToKelvin(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 32, want: 273.15},
+		// add more test cases here
+	}
+
+	for _, tc := range tests {
+		got := FahrenheitToKelvin(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("FahrenheitToKelvin(%v) = %v; want %v", tc.input, got, tc.want)
+		}
+	}
+}
+
+func TestCelsiusToKelvin(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 0, want: 273.15},
+		// add more test cases here
+	}
+
+	for _, tc := range tests {
+		got := CelsiusToKelvin(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("CelsiusToKelvin(%v) = %v; want %v", tc.input, got, tc.want)
+		}
+	}
+}
+
+func TestKelvinToCelsius(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 273.15, want: 0},
+		// add more test cases here
+	}
+
+	for _, tc := range tests {
+		got := KelvinToCelsius(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("KelvinToCelsius(%v) = %v; want %v", tc.input, got, tc.want)
