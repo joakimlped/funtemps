6ed13d6023a0874308e76c78489fde40429d9306
joakimlped <112881196+joakimlped@users.noreply.github.com>
2023-02-22 13:21:57 +0100

    Update conv.go

--- conv/conv.go
+++ conv/conv.go
@@ -35,7 +35,7 @@ func main() {
     funFacts := flag.String("funfacts", "", "Print fun facts about the Sun, Moon, or Earth")
     flag.Parse()
 
-    // Temperature conversion
+    // Temperatur konvertering
     var convertedTemp float64
     switch *output {
     case "F":
@@ -46,23 +46,23 @@ func main() {
         convertedTemp = KelvinToCelsius(*input)
     }
 
-    // Output temperature conversion result
+    // Output temperatur resultat
     fmt.Printf("%g%s is %g%s\n", *input, *output, convertedTemp, *output)
 
-    // Print fun facts if requested
+    // Print fun facts if requested, ikke fungerende enn så lenge
     if *funFacts != "" {
         switch *funFacts {
         case "sun":
-            fmt.Println("Temperature on the outer layer of the Sun is 5506.85°C.")
-            fmt.Println("Temperature in the core of the Sun is 15,000,000°C.")
+            fmt.Println("Temperatur på ytre lag av Solen er 5506.85°C.")
+            fmt.Println("Temperatur i Solens kjerne er 15,000,000°C.")
         case "moon", "luna":
-            fmt.Println("The average temperature on the Moon during the day is 107°C.")
-            fmt.Println("The average temperature on the Moon during the night is -153°C.")
+            fmt.Println("Temperatur på Månens overflate om dagen 107°C.")
+            fmt.Println("Temperatur på Månens overflate om natten er -153°C.")
         case "earth", "terra":
-            fmt.Println("The coldest temperature ever recorded on Earth was -89.2°C.")
-            fmt.Println("The hottest temperature ever recorded on Earth was 56.7°C.")
+            fmt.Println("Laveste temperatur målt på Jordens overflate er -89.2°C.")
+            fmt.Println("Høyeste temperatur målt på Jordens overflate er 56.7°C.")
         default:
-            fmt.Println("Unknown fun fact requested.")
+            fmt.Println("Uvisst funfact er forespurt")
         }
     }
 }
